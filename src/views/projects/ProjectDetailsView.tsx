96a1db0ac17f2d36a34ef74351369ca0a6c339ce
Allan <allan_hc@outlook.com>
2026-04-08 18:19:28 -0600

    Hasta video 533

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -2,6 +2,9 @@ import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { useQuery } from '@tanstack/react-query'
 import { getProjectById } from "@/api/ProjectAPI";
 import AddTaskModal from "@/components/tasks/AddTaskModal";
+import TaskList from "@/components/tasks/TaskList";
+import EditTaskData from "@/components/tasks/EditTaskData";
+import TaskModalDetails from "@/components/tasks/TaskModalDetails";
 
 export default function ProjectDetailsView() {
   const navigate = useNavigate()
@@ -9,7 +12,7 @@ export default function ProjectDetailsView() {
   const projectId = params.projectId!
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['editProject', projectId],
+    queryKey: ['project', projectId],
     queryFn: () => getProjectById(projectId),
     retry: false
   });
@@ -30,8 +33,10 @@ export default function ProjectDetailsView() {
           onClick={() => navigate(location.pathname + '?newTask=true')}
         >Agregar Tarea</button>
       </nav>
-
+      <TaskList tasks={data.tasks} />
       <AddTaskModal />
+      <EditTaskData />
+      <TaskModalDetails />
     </>
   )
 }
